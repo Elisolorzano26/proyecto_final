b1ce6ffd3506293d4e3a18ce6950a5e48177f8a1
Elisolorzano26 <msolorzanoo1@miumg.edu.gt>
2023-06-05 17:16:50 -0600

    subida del archivo config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+host = "localhost"
+usuario = "elizabeth"
+password = 123456
+baseDatos = "predicciones"
+claveSecreta = "92598f9d541d2eedf87dd2d088ea7846"
+
+urlConexion = f'mysql://{usuario}:{password}@{host}/{baseDatos}'
