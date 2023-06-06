f183b2496c2a4ec7d85515dd0238480d5d3fd875
Elisolorzano26 <msolorzanoo1@miumg.edu.gt>
2023-06-05 18:46:43 -0600

    modificacion de archivo app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask  # Importa la clase Flask del módulo flask
 from flask_sqlalchemy import SQLAlchemy  # Importa la clase SQLAlchemy del módulo flask_sqlalchemy
 from config import urlConexion, claveSecreta  # Importa las variables urlConexion y claveSecreta del archivo config.py
-from routes.rutas_usuarios import rutas_usuario  # Importa el blueprint rutas_usuario del módulo routes.rutas_usuarios
-from routes.rutas_prediccion import rutas_prediccion  # Importa el blueprint rutas_prediccion del módulo routes.rutas_prediccion
+from routes.rutas_usuarios import rutasUsuario  # Importa el blueprint rutas_usuario del módulo routes.rutas_usuarios
+from routes.rutas_prediccion import rutasPrediccion  # Importa el blueprint rutas_prediccion del módulo routes.rutas_prediccion
 
 app = Flask(__name__)  # Crea una instancia de la aplicación Flask
 
@@ -13,8 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactiva el seguimiento
 
 db = SQLAlchemy(app)  # Crea una instancia de la base de datos SQLAlchemy asociada a la aplicación
 
-app.register_blueprint(rutas_usuario)  # Registra el blueprint rutas_usuario en la aplicación
-app.register_blueprint(rutas_prediccion)  # Registra el blueprint rutas_prediccion en la aplicación
+app.register_blueprint(rutasUsuario)  # Registra el blueprint rutas_usuario en la aplicación
+app.register_blueprint(rutasPrediccion)  # Registra el blueprint rutas_prediccion en la aplicación
 
 with app.app_context():  # Crea un contexto de aplicación para ejecutar comandos dentro de la aplicación
     db.create_all()  # Crea todas las tablas en la base de datos según los modelos definidos
